c718f894082803e8a1feb6d3b32e6212d7d46ace
adam <adam@splickit.com>
2015-06-27 09:25:54 -0600

    better names override

--- app/overrides/do_not_show_taxon_products_unless_last_node.rb
+++ app/overrides/do_not_show_taxon_products_unless_last_node.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => 'spree/taxons/show',
-                     :name => 'do_not_show_taxon_products_unless_last_node',
+                     :name => 'do_not_show_taxon_products_unless_show_products_flag_is_set_on_taxon',
                      :surround_contents => '[data-hook="taxon_products"]',
                      :text => '<% if @taxon.show_products %><%= render_original %><% end %>');
 
