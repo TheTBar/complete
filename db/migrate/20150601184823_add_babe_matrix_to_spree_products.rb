51960ffc25a83197f11b56eaad383e35bdc8c7bc
adam <adam@splickit.com>
2015-07-01 09:26:29 -0600

    removing product personality migration from base app. putting it in the build your babe gem where it belongs

--- db/migrate/20150601184823_add_babe_matrix_to_spree_products.rb
+++
@@ -1,8 +0,0 @@
-class AddBabeMatrixToSpreeProducts < ActiveRecord::Migration
-  def change
-    add_column :spree_products, :vixen_value, :int
-    add_column :spree_products, :romantic_value, :int
-    add_column :spree_products, :flirt_value, :int
-    add_column :spree_products, :sophisticate_value, :int
-  end
-end
