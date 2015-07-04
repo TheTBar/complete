d98bf654b22ce9d3185f0663ba2066c13dd03503
adam <adam@splickit.com>
2015-07-04 13:09:49 -0600

    adding support for only showing sizes that are in stock

--- app/overrides/use_drop_down_for_product_variants.rb
+++ app/overrides/use_drop_down_for_product_variants.rb
@@ -7,3 +7,14 @@ Deface::Override.new(:virtual_path => 'spree/products/_cart_form',
     options_for_select(@product.variants_and_option_values_with_stock(current_currency).collect{ |v| ["#{variant_options(v)}  #{variant_price(v)}", v.id] })%>
 
 ');
+
+
+Deface::Override.new(:virtual_path => 'spree/products/_multi_item_cart_form_element',
+                     :name => 'use_drop_down_for_product_variants_in_package',
+                     :replace_contents => '[id="product-variants"]',
+                     :text => '
+                                  <h4 class="product-section-title">Sizes HELP</h4>
+                              <%= select_tag "variant_id",
+    options_for_select(@product.variants_and_option_values_with_stock(current_currency).collect{ |v| ["#{variant_options(v)}  #{variant_price(v)}", v.id] })%>
+
+');
