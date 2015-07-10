6d4cf1ac69d9805f7ffc44e8cd0e0463cfbd2042
adam <adam@splickit.com>
2015-07-10 06:43:51 -0600

    only returning products with stock

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -9,5 +9,18 @@ module Spree
       filtered_collection
     end
 
+    add_search_scope :in_stock do
+      # joins(variants_including_master: :stock_items).group('spree_products.id').having("SUM(count_on_hand) > 0")
+      joins(:master => :prices, variants: :stock_items).where("#{StockItem.quoted_table_name}.count_on_hand > 0").uniq
+    end
+    search_scopes << :in_stock
+
+    def self.available(available_on = nil, currency = nil)
+      # joins(:master => :prices).where("#{Product.quoted_table_name}.available_on <= ?", available_on || Time.now).joins(variants_including_master: :stock_items).group('spree_products.id').having("SUM(count_on_hand) > 0")
+      # joins(:master => :prices).where("#{Product.quoted_table_name}.available_on <= ?", available_on || Time.now)
+      joins(:master => :prices, variants: :stock_items).where("#{StockItem.quoted_table_name}.count_on_hand > 0 AND #{Product.quoted_table_name}.available_on <= ?", available_on || Time.now).uniq
+    end
+    search_scopes << :available
+
   end
 end
