42be729b668090cffa8924dbfb030c562f230a4b
adam <adam@splickit.com>
2015-07-12 14:19:01 -0600

    changing lables for main name

--- app/overrides/add_products_link_to_main_nav.rb
+++ app/overrides/add_products_link_to_main_nav.rb
@@ -1,6 +1,7 @@
 Deface::Override.new(:virtual_path => 'spree/shared/_main_nav_bar',
                      :name => 'add_products_and_themes_links',
                      :insert_after => "li#home-link",
-                     :text => "<li id=\"products-link\" data-hook><%= link_to Spree.t('products'), products_path %></li>
-                               <li id=\"themes-link\" data-hook><%= link_to Spree.t('themes'), '/t/themes' %></li>
+                     :text => "<li id=\"products-link\" data-hook><%= link_to Spree.t('Shop All Products'), products_path %></li>
+                               <li id=\"themes-link\" data-hook><%= link_to Spree.t('Explore Themed Collections'), '/t/themes' %></li>
+                               <li id=\"build-your-babe-link\" data-hook><%= link_to Spree.t('The Lingerie Concierge'), build_your_babe_path %></li>
 ")
