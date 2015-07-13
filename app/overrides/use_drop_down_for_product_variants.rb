4df14728117a0999603bd9a89f0a94d6624182b7
adam <adam@splickit.com>
2015-07-13 05:32:12 -0600

    adding selection for babes sizes on drop down for babes goods

--- app/overrides/use_drop_down_for_product_variants.rb
+++ app/overrides/use_drop_down_for_product_variants.rb
@@ -13,8 +13,10 @@ Deface::Override.new(:virtual_path => 'spree/products/_multi_item_cart_form_elem
                      :name => 'use_drop_down_for_product_variants_in_package',
                      :replace_contents => '[id="product-variants"]',
                      :text => '
-                                  <h4 class="product-section-title">Sizes HELP</h4>
-                              <%= select_tag "variant_id",
-    options_for_select(@product.variants_and_option_values_with_stock(current_currency).collect{ |v| ["#{variant_options(v)}  #{variant_price(v)}", v.id] })%>
-
+                 <h4 class="product-section-title">Sizes</h4>
+                 <% id = @product.variants_and_option_values_with_stock(current_currency).collect{ |v| ["#{variant_options(v)}", v.id] }.to_h[Spree::Babe.find(session[:babe_id]).size_value_for_size_option_type_name(@product.product_size_type)] if session.key?(:babe_id)%>
+                 <%= select_tag "variant_id",options_for_select(@product.variants_and_option_values_with_stock(current_currency).collect{ |v| ["#{variant_options(v)}  #{variant_price(v)}", v.id] }, selected: "#{id}")%>
 ');
+
+
+
