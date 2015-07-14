7bfbee8f51b5c892af1126ba4b80cdf45de43f42
adam <adam@splickit.com>
2015-07-14 09:22:56 -0600

    setting up for running rspec tests

--- Gemfile
+++ Gemfile
@@ -55,6 +55,10 @@ group :development, :test do
   gem 'quiet_assets'
 
   gem 'rspec-rails', '~> 3.0'
+  gem 'factory_girl_rails'
+  gem 'ffaker'
+  gem "capybara"
+
 
 end
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -40,6 +40,10 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  # config.include Spree::TestingSupport::Factories
+  # config.include FactoryGirl::Syntax::Methods
+  # config.include Spree::TestingSupport::UrlHelpers
+
 # The settings below are suggested to provide a good initial experience
 # with RSpec, but feel free to customize to your heart's content.
 =begin
