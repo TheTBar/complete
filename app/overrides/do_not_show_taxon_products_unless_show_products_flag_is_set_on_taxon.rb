51971b97307233f0e666bdf1c246d91dfdd6f6c0
adam <adam@splickit.com>
2015-07-14 14:09:54 -0600

    removing unused override

--- app/overrides/do_not_show_taxon_products_unless_show_products_flag_is_set_on_taxon.rb
+++ app/overrides/do_not_show_taxon_products_unless_show_products_flag_is_set_on_taxon.rb
@@ -2,9 +2,3 @@ Deface::Override.new(:virtual_path => 'spree/taxons/show',
                      :name => 'do_not_show_taxon_products_unless_show_products_flag_is_set_on_taxon',
                      :surround_contents => '[data-hook="taxon_products"]',
                      :text => '<% if @taxon.show_products %><%= render_original %><% end %>');
-
-
-Deface::Override.new(:virtual_path => 'spree/taxons/x_taxon',
-                     :name => 'show_taxon_icon',
-                     :replace => "erb[loud]:contains('spree/shared/products')",
-                     :text => '<%= link_to(image_tag(taxon.icon(:display)), show_package_path(taxon)) unless taxon.icon_file_name.nil? %>');
