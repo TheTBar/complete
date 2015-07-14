4535dbc649f3b719c8eeea0bb946010438be9dd3
adam <adam@splickit.com>
2015-07-14 09:29:07 -0600

    fixing index bug for variants

--- app/overrides/use_drop_down_for_product_variants.rb
+++ app/overrides/use_drop_down_for_product_variants.rb
@@ -15,7 +15,7 @@ Deface::Override.new(:virtual_path => 'spree/products/_multi_item_cart_form_elem
                      :text => '
                  <h4 class="product-section-title">Sizes</h4>
                  <% id = @product.variants_and_option_values_with_stock(current_currency).collect{ |v| ["#{variant_options(v).downcase}", v.id] }.to_h[Spree::Babe.find(session[:babe_id]).size_value_for_size_option_type_name(@product.product_size_type).downcase] if session.key?(:babe_id)%>
-                 <%= select_tag "variant_id",options_for_select(@product.variants_and_option_values_with_stock(current_currency).collect{ |v| ["#{variant_options(v)}  #{variant_price(v)}", v.id] }, selected: "#{id}")%>
+                 <%= select_tag "variant_id_#{index}",options_for_select(@product.variants_and_option_values_with_stock(current_currency).collect{ |v| ["#{variant_options(v)}  #{variant_price(v)}", v.id] }, selected: "#{id}")%>
 ');
 
 
