550bfdf5bfb76927f7f68e14c0925b7134d45bb6
adam <adam@splickit.com>
2015-07-21 15:28:11 -0600

    temporary override for focing babe values

---
+++ app/overrides/temporary_add_babe_fields_to_byb_form.rb
@@ -0,0 +1,9 @@
+Deface::Override.new(:virtual_path => 'spree/babes/_form',
+                     :name => 'temporary_babe_fields',
+                     :insert_before => "erb[loud]:contains('f.submit \"Show me the goods\"')",
+                     :text => "
+                      vixen<%= f.text_field :vixen_value %><br>
+                      romantic<%= f.text_field :romantic_value %><br>
+                      flirt<%= f.text_field :flirt_value %><br>
+                      sophisticate<%= f.text_field :sophisticate_value %><br>
+                    ");
