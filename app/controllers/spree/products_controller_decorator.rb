bab8c9f95ff70679487d9ffdcc87bdf48bca8fce
adam <adam@splickit.com>
2015-08-20 13:54:09 -0600

    taken out of package gem and expanded to get babe size stuff in there to work well with variant options package to preselect babes sizes

---
+++ app/controllers/spree/products_controller_decorator.rb
@@ -0,0 +1,19 @@
+module Spree
+  ProductsController.class_eval do
+
+    def show_package
+      @taxon = Taxon.friendly.find(params[:id])
+      return unless @taxon
+      @searcher = build_searcher(params.merge(taxon: @taxon.id, include_images: true))
+      # products = @searcher.retrieve_products
+      # @products = [products[0]]
+      @products = @searcher.retrieve_products
+      @pre_selected_sizes = []
+      if session.key?(:babe_id)
+        @products.each do |product|
+          @pre_selected_sizes.push(Spree::Babe.find(session[:babe_id]).size_value_for_size_option_type_name(product.product_size_type.name))
+        end
+      end
+    end
+  end
+end
