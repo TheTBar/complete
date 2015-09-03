ad0194405b5b08c70dfa7a50b0489aa5db6322f0
adam <adam@splickit.com>
2015-09-02 21:05:56 -0600

    working on product page and site background stuff

--- app/overrides/show_brand_on_product_details.rb
+++ app/overrides/show_brand_on_product_details.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => 'spree/products/show',
+Deface::Override.new(:virtual_path => 'spree/products/xshow',
                      :name => 'show_brand_on_product_details',
                      :insert_before => '[class="product-title"]',
-                     :text => '<h1 class="product-brand" itemprop="brand"><%= @product.brand %></h1>');
+                     :text => '<h2 class="product-brand" itemprop="brand"><%= @product.brand %></h2>');
