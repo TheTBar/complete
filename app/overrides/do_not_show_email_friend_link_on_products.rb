69d61718e18344d262946e112443afeed5dbc05f
adam <adam@splickit.com>
2015-09-08 15:55:01 -0600

    removing the email friend link on the product page

---
+++ app/overrides/do_not_show_email_friend_link_on_products.rb
@@ -0,0 +1,6 @@
+Deface::Override.new(
+    virtual_path: 'spree/products/show',
+    name: 'converted_product_description_351026984',
+    insert_bottom: '[data-hook="product_description"], #product_description[data-hook]',
+    text: ''
+)
