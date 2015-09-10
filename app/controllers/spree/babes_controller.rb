4e3303aadbb6c6875b35094ee4a498e2bdc7eddc
adam <adam@splickit.com>
2015-09-09 23:07:33 -0600

    removing cahing for build your babe

--- app/controllers/spree/babes_controller.rb
+++ app/controllers/spree/babes_controller.rb
@@ -3,8 +3,6 @@ module Spree
 
     before_action :set_babe, only: [:show, :edit, :update, :destroy]
 
-    caches_page :new
-
     def index
       @babes = Babe.where("spree_user_id = #{spree_current_user.id}")
     end
