c9b05015fdf3441ae49b89dcb7f22f314556c009
adam <adam@splickit.com>
2015-09-12 10:25:56 -0600

    removing bad code for image size and adding unused helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     return 'https://tbar-general.s3.amazonaws.com/images/'+image_name
   end
 
+  def getURLForProduct2ifItExistsIfNotGetProduct1(image)
+    if image.attachment.exists?(:product2)
+      return image.attachment.url(:product2)
+    else
+      return image.attachment.url(:product)
+    end
+  end
+
 end
