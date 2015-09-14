56df5a799422bffea5754ad3880cfa5a92804481
adam <adam@splickit.com>
2015-09-14 15:30:38 -0600

    adding loading methods for babe trait value

--- app/models/spree/babe_trait_value.rb
+++ app/models/spree/babe_trait_value.rb
@@ -1,5 +1,14 @@
 module Spree
   class BabeTraitValue < ActiveRecord::Base
     belongs_to :spree_babe_trait_type, :class_name => 'Spree::BabeTraitType'
+
+    def load_personality_traits(vixen,sophisticate,flirt,romantic)
+      self.vixen_value = vixen
+      self.sophisticate_value = sophisticate
+      self.flirt_value = flirt
+      self.romantic_value = romantic
+      self.save
+    end
+
   end
 end
