743d108e305663ebdbfd9ca19109ffd55b701cb3
adam <adam@splickit.com>
2015-09-14 22:58:57 -0600

    removing logging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,19 +8,14 @@ class ApplicationController < ActionController::Base
 
 
   def set_auth_cookie
-    Rails.logger.debug "WE ARE TESTING THE user_signed_in? in application helperr***************************"
     if spree_user_signed_in?
-      Rails.logger.debug "WE ARE SETTING THE COOKIE in application helperr***************************"
       cookies[:logged_in] = 'true'
     end
   end
 
   def assign_babe
-    Rails.logger.debug ("checking if user is signed in")
     if spree_user_signed_in?
-      Rails.logger.debug ("user is signed in, does session key exist for babe")
       if session.key?(:babe_id)
-        Rails.logger.debug "we have the babe"
         babe = Spree::Babe.find(session[:babe_id])
         babe.spree_user_id = spree_current_user.id
         babe.save
