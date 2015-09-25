2b5119ef6c9a5b1a3c44e93a7295bbb6a046fa48
adam <adam@splickit.com>
2015-09-24 18:41:55 -0600

    comments

--- app/models/spree/captcha_configuration.rb
+++ app/models/spree/captcha_configuration.rb
@@ -6,4 +6,9 @@ class Spree::CaptchaConfiguration < Spree::Preferences::Configuration
   preference :public_key,  :string,  default: '6LeygQ0TAAAAAB3ayjq7hPzBeAEh7k0t_kFmeXPr'
   preference :theme,       :string,  default: 'red'
   preference :use_captcha, :boolean, default: true
+
+  # can also run these commands from the rails console
+  #Spree::Captcha::Config.public_key = '6LeygQ0TAAAAAB3ayjq7hPzBeAEh7k0t_kFmeXPr'
+  #Spree::Captcha::Config.private_key = '6LeygQ0TAAAAAHIv8a-uBmdfpmkO81n5YgBtKb6A'
+
 end
