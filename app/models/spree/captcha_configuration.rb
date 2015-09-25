7914cf607a89e68487060d1760d96445477c388a
adam <adam@splickit.com>
2015-09-24 18:12:32 -0600

    adding captcha config file

---
+++ app/models/spree/captcha_configuration.rb
@@ -0,0 +1,9 @@
+class Spree::CaptchaConfiguration < Spree::Preferences::Configuration
+  THEMES = %w(red white blackglass clean custom)
+
+  # These keys works for localhost.
+  preference :private_key, :string,  default: '6LeygQ0TAAAAAHIv8a-uBmdfpmkO81n5YgBtKb6A'
+  preference :public_key,  :string,  default: '6LeygQ0TAAAAAB3ayjq7hPzBeAEh7k0t_kFmeXPr'
+  preference :theme,       :string,  default: 'red'
+  preference :use_captcha, :boolean, default: false
+end
