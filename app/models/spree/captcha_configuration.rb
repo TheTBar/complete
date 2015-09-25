9b72deb5b6006ff2bf16d0555ce83d0d4d1a413b
adam <adam@splickit.com>
2015-09-24 18:30:19 -0600

    setting value to true

--- app/models/spree/captcha_configuration.rb
+++ app/models/spree/captcha_configuration.rb
@@ -5,5 +5,5 @@ class Spree::CaptchaConfiguration < Spree::Preferences::Configuration
   preference :private_key, :string,  default: '6LeygQ0TAAAAAHIv8a-uBmdfpmkO81n5YgBtKb6A'
   preference :public_key,  :string,  default: '6LeygQ0TAAAAAB3ayjq7hPzBeAEh7k0t_kFmeXPr'
   preference :theme,       :string,  default: 'red'
-  preference :use_captcha, :boolean, default: false
+  preference :use_captcha, :boolean, default: true
 end
