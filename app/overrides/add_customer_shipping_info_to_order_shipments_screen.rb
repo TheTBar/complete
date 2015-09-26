ee9a8b27a2f74d093b19ccf1e33c4895646b6447
adam <adam@splickit.com>
2015-09-26 15:33:14 -0600

    adding debs view changes for completed orders

---
+++ app/overrides/add_customer_shipping_info_to_order_shipments_screen.rb
@@ -0,0 +1,16 @@
+Deface::Override.new(:virtual_path => 'spree/admin/orders/edit',
+                     :name => 'add_custom_shipping_info_to_order_shipments_screen',
+                     :insert_after => 'erb[loud]:contains("render partial: \'add_product\'")',
+                     :text => '<div class="panel panel-default" id="add-line-item" data-hook>
+  <div class="panel-heading">
+    <h1 class="panel-title">
+      Customer Shipping Information
+    </h1>
+  </div>
+  <div class="panel-body">
+    Name: <strong><%= @order.shipping_address.first_name %> <%= @order.shipping_address.last_name %></strong> &nbsp;&nbsp;&nbsp;&nbsp; Phone: <strong><%= @order.shipping_address.phone %></strong> &nbsp;&nbsp;&nbsp;&nbsp; Email: <strong><%= @order.email %></strong><p></p>
+    <strong><%= @order.shipping_address.address1 %><br>
+      <%= @order.shipping_address.address2+"<br>" unless @order.shipping_address.address2.blank? %>
+      <%= @order.shipping_address.city %>, <%= @order.shipping_address.state.abbr%> <%= @order.shipping_address.zipcode %></strong>
+  </div>
+</div>')
