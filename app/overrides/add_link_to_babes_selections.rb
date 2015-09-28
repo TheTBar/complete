598e7f191b752afecfc57cf411654b2510c655fd
adam <adam@splickit.com>
2015-09-28 10:32:53 -0600

    some better styling of buttons and layout on package

--- app/overrides/add_link_to_babes_selections.rb
+++ app/overrides/add_link_to_babes_selections.rb
@@ -1,13 +1,24 @@
 Deface::Override.new(:virtual_path => 'spree/products/show_package',
                      :name => 'add_link_to_babes_selections',
                      :insert_before => '[data-hook="package_left_part_wrap"]',
-                     :text => "<%= link_to Spree.t('Back To Babes Search Results'), my_babes_package_list_path(session['babe_id']) if session['babe_id'] %>"
+                     :text => '<div id="back-to-our-picks-for-babe-button" style="padding-top: 15px;">
+      <%= link_to "Back Our Picks For #{Spree::Babe.find(session[:babe_id]).display_name_babe}", my_babes_package_list_path(session[:babe_id]), :class => "btn btn-success our-picks-for-babe-button", :id => "see-our-pics" %>
+</div>'
                     );
 
 Deface::Override.new(:virtual_path => 'spree/orders/edit',
+                     :name => 'remove_or',
+                     :remove => "erb[loud]:contains('Spree.t(:or)')"
+                    );
+
+
+Deface::Override.new(:virtual_path => 'spree/orders/edit',
                      :name => 'add_link_to_babes_selections_at_cart',
-                     :insert_after => "erb[loud]:contains('link_to Spree.t(:continue_shopping)')",
-                     :text => "
-                      <%= Spree.t(:or) if session['babe_id'] %>
-                      <%= link_to Spree.t('Back To Babes Search Results'), my_babes_package_list_path(session['babe_id']) if session['babe_id'] %>"
+                     :replace => "erb[loud]:contains('link_to Spree.t(:continue_shopping)')",
+                     :text => '
+                      <% if session[:babe_id] %>
+    <div id="our-picks-for-babe-button">
+      <%= link_to "Back Our Picks For #{Spree::Babe.find(session[:babe_id]).display_name_babe}", my_babes_package_list_path(session[:babe_id]), :class => "btn btn-success", :id => "see-our-pics" %>
+</div>
+  <% end %>'
                     );
