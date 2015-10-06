16b2967e532d7ad5303cf221398f98b508a94e35
adam <adam@splickit.com>
2015-10-06 10:47:58 -0600

    fixing verbiage

--- app/overrides/add_link_to_babes_selections.rb
+++ app/overrides/add_link_to_babes_selections.rb
@@ -2,7 +2,7 @@ Deface::Override.new(:virtual_path => 'spree/products/show_package',
                      :name => 'add_link_to_babes_selections',
                      :insert_before => '[data-hook="package_left_part_wrap"]',
                      :text => '<div id="back-to-our-picks-for-babe-button" style="padding-top: 15px;">
-      <%= link_to "Back Our Picks For #{Spree::Babe.find(session[:babe_id]).display_name_babe}", my_babes_package_list_path(session[:babe_id]), :class => "btn btn-success our-picks-for-babe-button", :id => "see-our-pics" %>
+      <%= link_to "Back To Our Picks For #{Spree::Babe.find(session[:babe_id]).display_name_babe}", my_babes_package_list_path(session[:babe_id]), :class => "btn btn-success our-picks-for-babe-button", :id => "see-our-pics" %>
 </div>'
                     );
 
@@ -18,7 +18,7 @@ Deface::Override.new(:virtual_path => 'spree/orders/edit',
                      :text => '
                       <% if session[:babe_id] %>
     <div id="our-picks-for-babe-button">
-      <%= link_to "Back Our Picks For #{Spree::Babe.find(session[:babe_id]).display_name_babe}", my_babes_package_list_path(session[:babe_id]), :class => "btn btn-success", :id => "see-our-pics" %>
+      <%= link_to "Back To Our Picks For #{Spree::Babe.find(session[:babe_id]).display_name_babe}", my_babes_package_list_path(session[:babe_id]), :class => "btn btn-success", :id => "see-our-pics" %>
 </div>
   <% end %>'
                     );
