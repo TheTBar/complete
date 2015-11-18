ce5252fae1d41f092a288ff93a03f611e611120b
adam <adam@splickit.com>
2015-11-17 17:42:44 -0700

    adding logic to show link only if session babe id exists. duh

--- app/overrides/add_link_to_babes_selections.rb
+++ app/overrides/add_link_to_babes_selections.rb
@@ -1,9 +1,9 @@
 Deface::Override.new(:virtual_path => 'spree/products/show_package',
                      :name => 'add_link_to_babes_selections',
                      :insert_before => '[data-hook="package_left_part_wrap"]',
-                     :text => '<div id="back-to-our-picks-for-babe-button" style="padding-top: 15px;">
+                     :text => '<% if session[:babe_id] %><div id="back-to-our-picks-for-babe-button" style="padding-top: 15px;">
       <%= link_to "Back To Our Picks For #{Spree::Babe.find(session[:babe_id]).display_name_babe}", my_babes_package_list_path(session[:babe_id]), :class => "btn btn-success our-picks-for-babe-button", :id => "see-our-pics" %>
-</div>'
+</div> <%end%>'
                     );
 
 Deface::Override.new(:virtual_path => 'spree/orders/edit',
