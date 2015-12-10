8374e684c6c49fcb11d4e19c37f5d9c858a60a2a
adam <adam@splickit.com>
2015-12-10 09:44:23 -0700

    fixing spelling on tqween'

--- app/overrides/add_gift_note_to_cart_screen.rb
+++ app/overrides/add_gift_note_to_cart_screen.rb
@@ -2,7 +2,7 @@ Deface::Override.new(:virtual_path => 'spree/orders/_form',
                      :name => 'add_gift_note_to_order',
                      :insert_after => '[id="line_items"]',
                      :text => '<tr class="cart-gift-note">
-                                <td colspan="6" align="left">Add A Gift Note<span style="font-size: 10pt;padding-left: 15px;">(Handwritten by the TQueen herself)</span><br>
+                                <td colspan="6" align="left">Add A Gift Note<span style="font-size: 10pt;padding-left: 15px;">(Handwritten by the TQween herself)</span><br>
                                   <%= order_form.text_area :gift_note, cols: 40, rows: 4%>
                                 </td>
                               </tr>
